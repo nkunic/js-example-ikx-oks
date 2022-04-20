70b6b2a8c52169d4d8d7cab0fc71e44a060776b1
nkunic <natasa.kunic@gmail.com>
2022-04-20 22:16:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-example-ikx-oks-nk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-y835gy)
---
+++ index.js
@@ -0,0 +1,33 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+
+var xo = document.querySelector('.xo');
+
+createTable();
+
+var xoItem = document.querySelectorAll('.xo__item');
+var symbol = 'O';
+
+for (var i = 0; i < xoItem.length; i++) {
+  xoItem[i].addEventListener('click', insertSymbol);
+}
+
+function createTable() {
+  var text = '';
+  for (var i = 0; i < 9; i++) {
+    text += '<div class="xo__item"></div>';
+  }
+  xo.innerHTML = text;
+}
+
+function insertSymbol() {
+  // this.innerHTML = 'X';
+  if (symbol == 'O') {
+    symbol == 'X'
+  } else {
+    symbol == 'O'
+  }
+  this.innerHTML = symbol;
+}
